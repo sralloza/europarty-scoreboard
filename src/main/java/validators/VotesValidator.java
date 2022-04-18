bb9db37b2722b18ce523efab535ce3f89b711f0a
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-04-18 17:34:13 +0200

    fix: check for duplicate votes

--- src/main/java/validators/VotesValidator.java
+++ src/main/java/validators/VotesValidator.java
@@ -1,5 +1,6 @@
 package validators;
 
+import exceptions.DuplicateVoteException;
 import exceptions.ExcludedCountryException;
 import exceptions.JuryNotFoundException;
 import models.Jury;
@@ -7,7 +8,10 @@ import models.Participant;
 import models.Vote;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class VotesValidator {
 
@@ -16,6 +20,7 @@ public class VotesValidator {
          * 1. The jury must be registered in the list of juries
          * 2. Each country voted must be registered in the list of participants
          * 3. Each country voted must not be excluded from voting
+         * 4. Each jury must vote only once
          */
 
         juryVotes.forEach(vote -> {
@@ -42,5 +47,17 @@ public class VotesValidator {
                 }
             });
         });
+
+        List<String> duplicateVotes = juryVotes.stream()
+                .collect(Collectors.groupingBy(Vote::getJuryName))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue().size() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        if (!duplicateVotes.isEmpty()) {
+            throw new DuplicateVoteException(duplicateVotes);
+        }
     }
 }
