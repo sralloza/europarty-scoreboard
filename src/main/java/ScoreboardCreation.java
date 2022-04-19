d3137029108cb98165bd75927e7ad0b4023ace41
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-04-19 13:08:44 +0200

    refactor: get scoreboard name in service

--- src/main/java/ScoreboardCreation.java
+++ src/main/java/ScoreboardCreation.java
@@ -1,21 +1,13 @@
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import config.ConfigRepository;
 import services.ScoreWizService;
-
-import java.time.LocalDateTime;
 
 
 public class ScoreboardCreation {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new LocalModule());
         ScoreWizService scoreWizService = injector.getInstance(ScoreWizService.class);
-        ConfigRepository configRepository = injector.getInstance(ConfigRepository.class);
 
-        String scoreboardName = configRepository.getString("scorewiz.scoreboard.name");
-        if (configRepository.getBoolean("general.test")) {
-            scoreboardName += " - Test " + LocalDateTime.now();
-        }
-        scoreWizService.createScoreboard(scoreboardName);
+        scoreWizService.createScoreboard();
     }
 }
--- src/main/java/ScoreboardCreationByForm.java
+++ src/main/java/ScoreboardCreationByForm.java
@@ -1,20 +1,12 @@
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import config.ConfigRepository;
 import services.ScoreWizService;
-
-import java.time.LocalDateTime;
 
 public class ScoreboardCreationByForm {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new GoogleFormModule());
         ScoreWizService scoreWizService = injector.getInstance(ScoreWizService.class);
-        ConfigRepository configRepository = injector.getInstance(ConfigRepository.class);
 
-        String scoreboardName = configRepository.getString("scorewiz.scoreboard.name");
-        if (configRepository.getBoolean("general.test")) {
-            scoreboardName += " - Test " + LocalDateTime.now();
-        }
-        scoreWizService.createScoreboard(scoreboardName);
+        scoreWizService.createScoreboard();
     }
 }
