860b4464a2fb543d640d369add588ee5d152493d
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2023-05-12 23:41:56 +0200

    fix: constantly update html ids

--- chrome-extension/run.js
+++ chrome-extension/run.js
@@ -1,18 +1,39 @@
+const MAX_PARTICIPANTS = 100;
+
 function registerCustomCSS() {
-  var style = document.createElement("style");
-  html = "";
   items = document.getElementsByClassName("points");
   for (var i = 0; i < items.length; i++) {
     items[i].id = `points-${i}`;
+  }
+}
+
+function registerStyleInHtml() {
+  var style = document.createElement("style");
+  html = "";
+  for (var i = 0; i < MAX_PARTICIPANTS; i++) {
     html += `
     #points-${i} {
       font-size: 18px !important;
     }
     `;
   }
-  console.log(html);
   style.innerHTML = html;
   document.head.appendChild(style);
 }
 
-document.onload = registerCustomCSS();
+function registerEventListeners() {
+  $("#tools a").click(function (e) {
+    e.preventDefault();
+    registerCustomCSS();
+  });
+}
+
+function start() {
+  setInterval(() => {
+    registerCustomCSS();
+  }, 500);
+
+  registerStyleInHtml();
+}
+
+document.onload = start();
